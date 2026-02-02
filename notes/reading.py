fd36f1b9b34a2e8188910a15f0ee33b70126588b
CheesyDevil <seth.white@ucas-edu.net>
2026-02-02 10:49:30 -0700

    made reading

---
+++ notes/reading.py
@@ -0,0 +1,31 @@
+import csv
+
+try:
+    with open('notes\\sample.txt',"r") as file:
+        content=[]
+        for line in file:
+            content.append(line.strip())
+except:
+    print("File Not Found")
+else: #Else happens WITH try
+    for line in content:
+        print('hello',line)
+
+
+try:
+    with open("notes/Class CSV sample - Sheet1.csv",mode="r") as sample:
+        read=csv.reader(sample)
+        header=next(read)
+        users=[]
+        for line in read:
+            users.append(
+                {
+                    header[0]:line[0],
+                    header[1]:line[1]
+                }
+            )
+except:
+    print("not found")
+else:
+    for user in users:
+        print(user)

682605522c5263cb75f6d4f2048d1b8d1deecf87
CheesyDevil <seth.white@ucas-edu.net>
2026-03-18 10:51:02 -0600

    started pet simulator

---
+++ individual projects/Pet Simulator/Pet simulator/helpers.py
@@ -0,0 +1,39 @@
+#Change stat function(Health, Level, Health, Hunger, Happiness, Energy)
+
+#time passing
+    #covert hours to days and over time increase pet age
+
+
+#Feed
+    #Buy From Food options
+    #increase stat function
+    # return To menu
+
+#play
+    #choose how long you play for
+    #increase happiness and health
+    #decrease energy
+
+
+#Put to Sleep
+    #choose how long you nap for
+    #increase Energy and health
+    #decrease Hunger
+
+#Check status
+    #Bar Graph Function
+    #print out name, species, age, Level, Health, Hunger, Happiness, Energy, Skills
+    
+
+#Pet management
+    #return to pet select 
+    #Create new Pet
+    #Switch pet
+    #Release pet
+    #return to Main Menu
+
+#Save
+    #save stats to the pet's area of save file
+
+#Load
+    #takes The pet info and opens it
